a3af4c7f6de14008eef0a2347b65aa2f48c4be55
Zatara-Wrought <zatara.forever@gmail.com>
2018-11-30 09:25:15 -0800

    MVP

--- ANSWERS.md
+++ ANSWERS.md
@@ -2,8 +2,22 @@
 
 1. What is the purpose of using _sessions_?
 
+To create state
+
 2. What does bcrypt do to help us store passwords in a secure manner.
+
+Allows developers to hash and salt passwords.
 
 3. What does bcrypt do to slow down attackers?
 
+Implements a slow hashing algorithm as a deterrent and obstacle.
+
 4. What are the three parts of the JSON Web Token?
+
+Header, payload, and signature.
+
+Header = token type
+
+Payload = claims = data [registered, public, private]
+
+Signature = proof that nothing was changed during transit
--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,77 @@
-const axios = require('axios');
-
-const { authenticate } = require('./middlewares');
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig.js");
+const jwtKey = require("../_secrets/keys").jwtKey;
+const { authenticate } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const secret = jwtKey;
+  const options = {
+    expiresIn: "1m"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  if (!creds.username || !creds.password) {
+    const errorMessage = "Please provide both a username and password";
+    res.status(400).json({ errorMessage });
+    return;
+  }
+  const hash = bcrypt.hashSync(creds.password, 4);
+  creds.password = hash;
+
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+  if (!creds.username || !creds.password) {
+    const errorMessage = "Please provide both a username and password";
+    res.status(400).json({ errorMessage });
+    return;
+  }
+  db("users")
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: "welcome!", token });
+      } else {
+        res.status(401).json({ message: "you shall not pass!!" });
+      }
+    })
+    .catch(err => res.json(err));
 }
 
 function getJokes(req, res) {
   axios
-    .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
-    )
+    .get("https://safe-falls-22549.herokuapp.com/random_ten")
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
